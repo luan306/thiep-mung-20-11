c692ab49c869c2dd9f08fe2e5b1a02906fac3c58
luan306 <nguyenluan3062001@gmail.com>
2021-11-10 22:00:12 +0700

    first commit

---
+++ thiep-mung-main/config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Thầy Cô';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-11';
+const titleCard = 'Tặng Tất Cả Thầy Cô';
+const contentCard = 'Chúc mừng thầy cô nhân Ngày Nhà giáo Việt Nam 20/11. Ở nơi phương trời xa, em xin kính chúc các thầy, các cô luôn vui khỏe, hạnh phúc. Chúc cho mọi lời chúc của tất cả học trò dành cho thầy cô đều trở thành hiện thực.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
